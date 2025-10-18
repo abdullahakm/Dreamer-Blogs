947f7b209042eaba82b732da5ec5c8b95ea14bd5
Abdullah Awais <abdullah.awais@wicked.digital>
2025-10-18 23:31:30 +0500

    1-Add service extension class

---
+++ Dreamer Blogs/Dreamer.Blog.API/Extensions/ServiceExtension.cs
@@ -0,0 +1,14 @@
+﻿namespace Dreamer.Blog.API.Extensions;
+
+internal static class ServiceExtension
+{
+    public static void AddApplicationServices(this IServiceCollection services) => services.AddScoped<IExampleService, ExampleService>();// Add custom application services here
+}
+
+internal class ExampleService : IExampleService
+{
+}
+
+internal interface IExampleService
+{
+}
